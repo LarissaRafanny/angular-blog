0733b311265cb798db6be3b690a92fa5dc39eac0
larissarafanny <larissarafanny.martins@gmail.com>
2023-10-15 13:23:08 -0300

    components createds with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardsComponent,
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
