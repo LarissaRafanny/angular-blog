56997d25ce66d9e708bd3ce69963cbf535680218
larissarafanny <larissarafanny.martins@gmail.com>
2023-10-15 15:21:17 -0300

    components dinamycs propertys

--- src/app/components/small-cards/small-cards.component.ts
+++ src/app/components/small-cards/small-cards.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-cards',
   templateUrl: './small-cards.component.html',
   styleUrls: ['./small-cards.component.css']
 })
-export class SmallCardsComponent {
+export class SmallCardsComponent implements OnInit {
 
+  @Input()
+  photoCover: string=""
+  @Input()
+  cardTitle: string=""
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 }
+
+
+
+
+
+
+
